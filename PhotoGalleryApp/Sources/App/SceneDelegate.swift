8f14ff36e4a128fa686fc7baae45c475be74494c
Vitaliy <vmolokov59@gmail.com>
2023-01-31 15:59:15 +0300

    Preparation

---
+++ PhotoGalleryApp/Sources/App/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  PhotoGalleryApp
+//
+//  Created by Виталик Молоков on 31.01.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let viewController = ViewController()
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+    }
+}
+
