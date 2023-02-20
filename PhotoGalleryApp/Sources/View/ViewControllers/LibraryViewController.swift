95f84512824c76f815388c755686ac6ac70beadb
Vitaliy <vmolokov59@gmail.com>
2023-02-20 14:28:17 +0300

    No comments

---
+++ PhotoGalleryApp/Sources/View/ViewControllers/LibraryViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LibraryViewController.swift
+//  PhotoGalleryApp
+//
+//  Created by Виталик Молоков on 19.02.2023.
+//
+
+import UIKit
+
+class LibraryViewController: UIViewController {
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray2
+    }
+}
