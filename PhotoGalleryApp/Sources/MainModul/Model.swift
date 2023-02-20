89fc918ae745d01b2ebb0ae37601a83cde832ac6
Vitaliy <vmolokov59@gmail.com>
2023-02-20 15:24:58 +0300

    1

--- PhotoGalleryApp/Sources/MainModul/Model.swift
+++ PhotoGalleryApp/Sources/MainModul/Model.swift
@@ -8,36 +8,35 @@
 import UIKit
 
 struct CompositionalModel: Hashable {
-    var mainTitle: String?
     var description: String?
     var image: UIImage?
 }
 
 extension CompositionalModel {
     static let modelsArray = [
-        [CompositionalModel(mainTitle: nil, description: nil, image: UIImage(named: "Recents")),
-         CompositionalModel(mainTitle: nil, description: nil, image: UIImage(named: "Favourites")),
-         CompositionalModel(mainTitle: nil, description: nil, image: UIImage(named: "GoPro")),
-         CompositionalModel(mainTitle: nil, description: nil, image: UIImage(named: "DJI")),
-         CompositionalModel(mainTitle: nil, description: nil, image: UIImage(named: "Instagram")),
-         CompositionalModel(mainTitle: nil, description: nil, image: UIImage(named: "WhatsApp"))
+        [CompositionalModel(description: nil, image: UIImage(named: "Recents")),
+         CompositionalModel(description: nil, image: UIImage(named: "Favourites")),
+         CompositionalModel(description: nil, image: UIImage(named: "GoPro")),
+         CompositionalModel(description: nil, image: UIImage(named: "DJI")),
+         CompositionalModel(description: nil, image: UIImage(named: "Instagram")),
+         CompositionalModel(description: nil, image: UIImage(named: "WhatsApp"))
         ],
-        [CompositionalModel(mainTitle: nil, description: "Videos", image: UIImage(systemName: "video")),
-         CompositionalModel(mainTitle: nil, description: "Selfies", image: UIImage(systemName: "person.crop.square")),
-         CompositionalModel(mainTitle: nil, description: "Live Photo", image: UIImage(systemName: "livephoto")),
-         CompositionalModel(mainTitle: nil, description: "Portrait", image: UIImage(systemName: "cube")),
-         CompositionalModel(mainTitle: nil, description: "Panoramas", image: UIImage(systemName: "pano")),
-         CompositionalModel(mainTitle: nil, description: "Slo-mo", image: UIImage(systemName: "slowmo")),
-         CompositionalModel(mainTitle: nil, description: "Cinematic", image: UIImage(systemName: "video.square")),
-         CompositionalModel(mainTitle: nil, description: "Bursts", image: UIImage(systemName: "square.3.layers.3d.down.right")),
-         CompositionalModel(mainTitle: nil, description: "Screenshots", image: UIImage(systemName: "dot.circle.viewfinder")),
-         CompositionalModel(mainTitle: nil, description: "Screen Recordings", image: UIImage(systemName: "smallcircle.filled.circle")),
-         CompositionalModel(mainTitle: nil, description: "Animated", image: UIImage(systemName: "square.3.layers.3d.top.filled"))
+        [CompositionalModel(description: "Videos", image: UIImage(systemName: "video")),
+         CompositionalModel(description: "Selfies", image: UIImage(systemName: "person.crop.square")),
+         CompositionalModel(description: "Live Photo", image: UIImage(systemName: "livephoto")),
+         CompositionalModel(description: "Portrait", image: UIImage(systemName: "cube")),
+         CompositionalModel(description: "Panoramas", image: UIImage(systemName: "pano")),
+         CompositionalModel(description: "Slo-mo", image: UIImage(systemName: "slowmo")),
+         CompositionalModel(description: "Cinematic", image: UIImage(systemName: "video.square")),
+         CompositionalModel(description: "Bursts", image: UIImage(systemName: "square.3.layers.3d.down.right")),
+         CompositionalModel(description: "Screenshots", image: UIImage(systemName: "dot.circle.viewfinder")),
+         CompositionalModel(description: "Screen Recordings", image: UIImage(systemName: "smallcircle.filled.circle")),
+         CompositionalModel(description: "Animated", image: UIImage(systemName: "square.3.layers.3d.top.filled"))
         ],
-        [CompositionalModel(mainTitle: nil, description: "Imports", image: UIImage(systemName: "square.and.arrow.down")),
-         CompositionalModel(mainTitle: nil, description: "Dublicates", image: UIImage(systemName: "rectangle.on.rectangle")),
-         CompositionalModel(mainTitle: nil, description: "Hidden", image: UIImage(systemName: "eye.slash")),
-         CompositionalModel(mainTitle: nil, description: "Recently Deleted", image: UIImage(systemName: "trash"))
+        [CompositionalModel(description: "Imports", image: UIImage(systemName: "square.and.arrow.down")),
+         CompositionalModel(description: "Dublicates", image: UIImage(systemName: "rectangle.on.rectangle")),
+         CompositionalModel(description: "Hidden", image: UIImage(systemName: "eye.slash")),
+         CompositionalModel(description: "Recently Deleted", image: UIImage(systemName: "trash"))
         ]
     ]
 }
